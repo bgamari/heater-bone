fc55cc43b01807d0de9a0a7e5f7fb2173f0d836e
Ben Gamari <bgamari.foss@gmail.com>
2013-11-15 23:26:36 +0000

    LcdTest: Updates found on BeagleBone

--- code/LcdTest.hs
+++ code/LcdTest.hs
@@ -1,4 +1,8 @@
-import Control.Monad (forever)
+import Control.Error
+import Control.Concurrent (threadDelay)
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Maybe
 import qualified LCD
 import LCD (LCD, LCDPins(..))
@@ -11,9 +15,15 @@ lcdPins = LCDPins { rsPin  = 81
 
 backlight = "heater:white:lcd"
 
+readTemp :: EitherT String IO Double
+readTemp = do
+    v <- liftIO $ readFile "/sys/class/hwmon/hwmon0/device/temp2_input"
+    (/1000) <$> tryRead "readTemp: Parse error" v
+
 main = do 
     lcd <- LCD.open lcdPins
     Just bklt <- runMaybeT $ LED.open backlight
+    writeFile "/sys/class/hwmon/hwmon0/device/channel_sel" "ext\n"
     LED.setBrightness (LED.brightness 0.9) bklt
     LCD.queue lcd $ do
       LCD.home
@@ -21,8 +31,14 @@ main = do
       LCD.setCursor False
       LCD.setBlink False
       LCD.setDisplay True
-    forever $ getLine >>= \l->LCD.queue lcd $ do
-        LCD.home
-        LCD.clear
-        LCD.write l
+    forever $ runEitherT $ do
+      a <- readTemp
+      liftIO $ setLCD lcd $ show a++"deg C"
+      liftIO $ threadDelay (1000*1000)
+
+setLCD :: LCD -> String -> IO ()
+setLCD lcd s = LCD.queue lcd $ do
+    LCD.home
+    LCD.clear
+    LCD.write s
 
